ccf81476f2d4d73db74ec0b9de127defc7455fae
ArcaneWD <harsha2004alpur@gmail.com>
2025-07-29 21:56:39 +0530

    Initial Commit for Portfolio Website

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Button = ({ text, className, id }) => {
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        const target = document.getElementById('work');
+        if (target && id) {
+            const offset = window.innerHeight * 0.15;
+            const top = target.getBoundingClientRect().top + window.scrollY - offset;
+
+            window.scrollTo({ top, behavior: 'smooth' });
+        }
+    };
+
+    return (
+        <a className={`${className ?? ''} cta-wrapper`} onClick={handleClick} href="#">
+            <div className="cta-button group">
+                <div className="bg-circle" />
+                <p className="text">{text}</p>
+                <div className="arrow-wrapper">
+                    <img src="/images/arrow-down.svg" alt="arrow" />
+                </div>
+            </div>
+        </a>
+    );
+};
+
+export default Button;
