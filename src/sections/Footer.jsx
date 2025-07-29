3f98afa678593d6af4fded5c677d4e5c4c047240
ArcaneWD <harsha2004alpur@gmail.com>
2025-07-29 22:13:43 +0530

    Changing footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
                     <p>Terms & Conditions</p>
                 </div>
                 <div className="socials">
-                    {socialImgs.map((socialImg, index) => (
-                        <div key={index} className="icon">
-                            <img src={socialImg.imgPath} alt="social icon" />
-                        </div>
-                    ))}
+                    {/*{socialImgs.map((socialImg, index) => (*/}
+                    {/*    <div key={index} className="icon">*/}
+                    {/*        <img src={socialImg.imgPath} alt="social icon" />*/}
+                    {/*    </div>*/}
+                    {/*))}*/}
                 </div>
                 <div className="flex flex-col justify-center">
                     <p className="text-center md:text-end">
-                        © {new Date().getFullYear()} A Harshavardhan Reddy. All rights reserved.
+                        © {new Date().getFullYear()} Harsha. All rights reserved.
                     </p>
                 </div>
             </div>
